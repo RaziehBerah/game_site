cf526576f9ce7654f7c996946c9cc6798f4d5c7c
Razieh <razieh_berah@yahoo.com>
2025-07-07 19:01:18 +0330

    CroppedImage

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,22 +1,17 @@
-import React from 'react'
-import { Game } from '../entities'
-import { Card, CardBody, Heading, Img, Text } from '@chakra-ui/react'
-interface Props{
-    game:Game
+import { Card, CardBody, Heading, Img } from "@chakra-ui/react";
+import { Game } from "../entities";
+interface Props {
+  game: Game;
 }
-const GameCard = ({game}:Props) => {
+const GameCard = ({ game }: Props) => {
   return (
     <Card overflow={"hidden"} borderRadius={"10px"}>
-        <Img src={game.background_image} />
-        <CardBody>
-            <Heading fontSize={"xl"}>
-{game.name}
-            </Heading>
-
-            
-        </CardBody>
+      <Img src={game.background_image} />
+      <CardBody>
+        <Heading fontSize={"xl"}>{game.name}</Heading>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
