86fe41aa2be728a75c441388fbb5681054078e4c
Razieh <razieh_berah@yahoo.com>
2025-07-07 18:23:01 +0330

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
     >
-      <GridItem area={"nav"} bgColor={"green"}>
-        Navbar
+      <GridItem area={"nav"} >
+        <Navbar />
       </GridItem>
       <Show above="lg">
         <GridItem area={"aside"} bgColor={"yellow"}>
---
+++ src/components/SwitchMode.tsx
@@ -0,0 +1,17 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const SwitchMode = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  return (
+    <HStack>
+      <Switch
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+        colorScheme="green"
+      />
+      <Text whiteSpace="normal">Dark Mod</Text>
+    </HStack>
+  );
+};
+
+export default SwitchMode;
