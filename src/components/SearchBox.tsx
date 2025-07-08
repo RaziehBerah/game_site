26336fae3feebd06d9b5cd64f0ffc534597cefc4
Razieh <razieh_berah@yahoo.com>
2025-07-08 17:14:14 +0330

    filter by searchtext

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,15 +1,29 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
 import { CiSearch } from "react-icons/ci";
+import usegameStoreQuery from "../store";
 
 const SearchBox = () => {
+  const ref = useRef<HTMLInputElement>(null);
+  const setSearchText = usegameStoreQuery((s) => s.setSearchText);
   return (
-    <form style={{ width: "100%" }}>
+    <form
+      style={{ width: "100%" }}
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) {
+          setSearchText(ref.current?.value);
+          ref.current.value = "";
+        }
+      }}
+    >
       <InputGroup>
         <InputLeftElement children={<CiSearch />} />
         <Input
           placeholder="Search..."
           borderRadius={"15px"}
           variant={"filled"}
+          ref={ref}
         />
       </InputGroup>
     </form>
